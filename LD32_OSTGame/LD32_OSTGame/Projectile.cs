118d306efe718f8a416c7e8687260b8da1ea9f18
Aaron "F1tZy" Van Prooyen <vanproyaa@ost-cnu2341dwq.ostusa.com>
2015-04-18 10:07:24 -0400

    pink is awsome

---
+++ LD32_OSTGame/LD32_OSTGame/Projectile.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD32_OSTGame
+{
+    public class Projectile : Entity
+    {
+        public int Damage { get; set; }
+    }
+}
