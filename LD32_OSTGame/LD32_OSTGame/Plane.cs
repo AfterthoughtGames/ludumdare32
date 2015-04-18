00ed2de1e5145c7fc06b73c46f5786326ca1c28e
Matt Johnson <mpjohnson@ostusa.com>
2015-04-18 10:13:58 -0400

    Added Plane class

---
+++ LD32_OSTGame/LD32_OSTGame/Plane.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD32_OSTGame
+{
+    public class Plane : Entity
+    {
+
+        protected float fireRate { get; set; }
+
+        public Plane(Texture2D image) : base()
+        {
+            
+        }
+    }
+}
