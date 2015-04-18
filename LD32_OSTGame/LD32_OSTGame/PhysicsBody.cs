84b92fc99784d64c24b424fb4369b739855906dd
Larry Martin <magicrat@gmail.com>
2015-04-18 10:46:34 -0400

    physics goodies{

---
+++ LD32_OSTGame/LD32_OSTGame/PhysicsBody.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LD32_OSTGame
+{
+    public class PhysicsBody
+    {
+        public Rectangle BoundingBox { get; set; }
+        public Vector2 Position { get; set; }
+    }
+}
