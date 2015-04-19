38525e2aefa1cc9f2f63aa61a7eaa6f89fe87c31
Matt Johnson <mpjohnson@ostusa.com>
2015-04-19 09:19:56 -0400

    Add tape powerup class

---
+++ LD32_OSTGame/LD32_OSTGame/Tape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace LD32_OSTGame
+{
+    public class Tape : PowerUp
+    {
+        public void LoadContent(ContentManager cm)
+        {
+            this.image = cm.Load<Texture2D>("thing");
+        }
+    }
+}
