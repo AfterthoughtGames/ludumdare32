3d4bf0c5fd6b4991b0f17c96ed2b59e01f36f431
Larry Martin <magicrat@gmail.com>
2015-04-19 16:20:13 -0400

    razor collide work

--- LD32_OSTGame/LD32_OSTGame/Ball.cs
+++ LD32_OSTGame/LD32_OSTGame/Ball.cs
@@ -92,6 +92,69 @@ namespace LD32_OSTGame
                     }
                 }
             }
+
+            if (collidedWith.GetType() == typeof(RazorEnt))
+            {
+                Vector2 away = collidedWith.Position - Position;
+                if (away.Length() < 140 * Scale)
+                {
+                    //collidedWith.Health--;
+                    this.Health--;
+                    Game1.Hit.Play();
+
+
+                    for (int i = 0; i < 5; i++)
+                    {
+
+                        Particle p = new Particle(Game1.Particle1, collidedWith.Position + new Vector2(rand.Next(-10, 10), rand.Next(-10, 10)), .5f * (float)rand.NextDouble(),
+                            new Vector2(rand.Next(-5, 5), rand.Next(-5, 5)), (float)rand.NextDouble(), rand.Next(10, 20), new Color(rand.Next(0, 255), rand.Next(0, 255), rand.Next(0, 255), 1));
+                        Game1.Particles.Add(p);
+                    }
+
+                    if (this.Health == 0)
+                    {
+                        Game1.Rip.Play();
+
+                        Game1.Score++;
+
+                        //should probably do some sort of health check???
+                        Game1.BallCount--;
+
+                        if (this.Scale > 0.125)
+                        {
+
+                            //random powerup placment
+                            Random rndPower = new Random();
+
+                            int rndNum = rndPower.Next(100);
+
+                            if (rndNum < 20)
+                            {
+                                Game1.Entites.Add(new Razor(Game1.razorImg, this.Position, 1, 0));
+                            }
+
+                            Ball ball1 = new Ball(image, 3, new Vector2(Position.X, Position.Y), (Scale / 2),
+                            new Vector2((float)rand.Next(-200, 200), (float)rand.Next(-200, 200)), (float)rand.Next(-100, 100) / 100.0f);
+
+                            ball1.Body = this.ReturnNewBodyByScale(ball1.Scale); //new Rectangle((int)Position.X, (int)Position.Y, (int)(this.image.Width * ball1.Scale), (int)(this.image.Height * ball1.Scale));
+
+                            Game1.Entites.Add(ball1);
+
+                            Ball ball2 = new Ball(image, 3, new Vector2(Position.X, Position.Y), (Scale / 2),
+                            new Vector2((float)rand.Next(-200, 200), (float)rand.Next(-200, 200)), (float)rand.Next(-100, 100) / 100.0f);
+
+                            ball2.Body = this.ReturnNewBodyByScale(ball2.Scale); //new Rectangle((int)Position.X, (int)Position.Y, (int)(this.image.Width * ball2.Scale), (int)(this.image.Height * ball2.Scale));
+
+                            Game1.Entites.Add(ball2);
+
+
+
+                            Game1.BallCount++;
+                            Game1.BallCount++;
+                        }
+                    }
+                }
+            }
         }
 
         public override void Draw(GameTime gameTime, SpriteBatch batch)
